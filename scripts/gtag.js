bd39cb89acf527c001b9a8adf9a986bab258ec3c
Leskov <igor.leskov.m@mail.ru>
2025-06-21 10:13:32 +0300

    Create gtag.js

---
+++ scripts/gtag.js
@@ -0,0 +1,8 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-NYMTZ8N93D"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-NYMTZ8N93D');
+</script>
